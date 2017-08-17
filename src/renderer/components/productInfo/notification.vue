0af2dd71cf1ad58cc364215ae4ef3171e2b1ad35
jeodiong <jeodiong@hotmail.com>
2017-08-17 22:42:09 +0800

    crap

--- src/renderer/components/productInfo/notification.vue
+++ src/renderer/components/productInfo/notification.vue
@@ -13,44 +13,71 @@ export default {
   data () {
     return {
       allData: [],
-      allDataRead: [],
-      selected: '',
+      curPost: {},
       isEdit: false,
-      curPost: {}
+      selected: '',
+      type: 0,
+      nextPageNo: 1,
+      pageSize: this.$api.pageSize
     }
   },
   created () {
-    this.getAllData(false, 'allData')
-    this.getAllData(true, 'allDataRead')
+    this.changeType(0)
     this.$nextTick(() => {
       window.addEventListener('click', () => {
         this.isEdit = false
       })
     })
+    this.busEventRouteName()
   },
   methods: {
-    getAllData (isRead, valName) {
+    busEventRouteName () {
+      this.$bus.off(this.$route.name)
+      this.$bus.once(this.$route.name, content => {
+        this.isEdit = false
+      })
+    },
+    reset () {
+      this.allData = []
+      this.curPost = {}
+      this.isEdit = false
+      this.selected = ''
+      this.type = 0
+      this.nextPageNo = 1
+      this.pageSize = this.$api.pageSize
+    },
+    getAllData () {
       let url = this.$api.logMy
       let body = {
         data: {
-          isRead: isRead,
-          productId: this.$route.params.id
+          productId: this.$route.params.id,
+          nextPageNo: this.nextPageNo,
+          pageSize: this.pageSize,
+          isRead: this.type
         }
       }
       this.$http.post(url, body).then((res) => {
         if (res.data.code === 0) {
-          let allData = res.data.data.allData
-          this[valName] = this.$_.chain(allData).map((obj) => {
+          let { allData, nextPageNo } = res.data.data
+          allData = this.$_.chain(allData).map((obj) => {
             obj.createdAt = this.$moment(obj.createdAt).format('YYYY/MM/DD h:mm:ss')
             return obj
           }).value()
+          this.allData = this.allData.concat(allData)
+          this.nextPageNo = nextPageNo
         }
       })
     },
     handleEdit (item) {
+      this.busEventRouteName()
       this.selected = item._id
       this.isEdit = true
       this.curPost = item.postId
+    },
+    changeType (type) {
+      this.reset()
+      this.type = type
+      this.getAllData()
     }
   }
 }
